27979d50d4f1623509e9a729f8baa5e0980932ab
Unknown <luizfernandovieira_cf@hotmail.com>
2018-05-09 22:04:07 -0300

    add login

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,12 @@
+class AuthenticationController < ApplicationController
+
+  # render  :login
+  # layout :login
+  # layout 'login'
+  # layout :home
+
+  def login
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :shelves
   resources :books
   resources :users
+
   root to: "application#home"
+
+  get 'login', to: "authentication#login"
 
 end
