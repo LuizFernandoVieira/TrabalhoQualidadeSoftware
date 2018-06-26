a6236166988abe2b439445924f0b352ff76a9335
Kilmer Luiz Aleluia <kilmerluizale@gmail.com>
2018-06-25 22:14:47 -0300

    consult_books#index spec.

--- spec/controllers/consult_books_controller_spec.rb
+++ spec/controllers/consult_books_controller_spec.rb
@@ -1,7 +1,21 @@
-# require 'test_helper'
-#
-# class ConsultBooksControllerSpec < ActionDispatch::IntegrationTest
-#   # test "the truth" do
-#   #   assert true
-#   # end
-# end
+require 'rails_helper'
+
+RSpec.describe ConsultBooksController, type: :controller do
+  before :all do
+    @user = FactoryBot.create :user
+    @user2 = FactoryBot.create :user
+    FactoryBot.create(:book, shelf_id: @user.shelf.id, title: 'Only One')
+    FactoryBot.create_list(:book, 5, shelf_id: @user.shelf.id)
+    FactoryBot.create_list(:book, 7, shelf_id: @user2.shelf.id)
+  end
+
+  describe 'index' do
+    it 'should return all user books' do
+      get :index, session: {'user_id': @user.id}
+      expect(assigns[:books].count).to eq Book.count
+      get :index, session: {'user_id': @user.id}, params: {search: 'Only One'}
+      expect(assigns[:books].count).to eq 1
+    end
+  end
+
+end
