f2748dc944be15b44ccef68b7289ae817a8a68d0
Kilmer Luiz Aleluia <kilmerluizale@gmail.com>
2018-06-25 22:19:55 -0300

    Updated test.

--- spec/controllers/consult_books_controller_spec.rb
+++ spec/controllers/consult_books_controller_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe ConsultBooksController, type: :controller do
   before :all do
     @user = FactoryBot.create :user
     @user2 = FactoryBot.create :user
-    FactoryBot.create(:book, shelf_id: @user.shelf.id, title: 'Only One')
     FactoryBot.create_list(:book, 5, shelf_id: @user.shelf.id)
     FactoryBot.create_list(:book, 7, shelf_id: @user2.shelf.id)
   end
@@ -13,8 +12,8 @@ RSpec.describe ConsultBooksController, type: :controller do
     it 'should return all user books' do
       get :index, session: {'user_id': @user.id}
       expect(assigns[:books].count).to eq Book.count
-      get :index, session: {'user_id': @user.id}, params: {search: 'Only One'}
-      expect(assigns[:books].count).to eq 1
+      get :index, session: {'user_id': @user.id}, params: {search: 'Harry Potter 1'}
+      expect(assigns[:books].count).to eq Book.where('title LIKE ?', '%Harry Potter 1%').count
     end
   end
 
