496cfcd2db1db10edd59007acae0cde9f1e7e599
Kilmer Luiz Aleluia <kilmerluizale@gmail.com>
2018-06-25 21:36:55 -0300

    books#create spec.

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 
-RSpec.describe BooksController, type: :controller  do
+RSpec.describe BooksController, type: :controller do
   before :all do
     @user = FactoryBot.create :user
   end
@@ -9,9 +9,25 @@ RSpec.describe BooksController, type: :controller  do
   describe 'index' do
     it 'should return all user books' do
       FactoryBot.create_list(:book, 5, shelf_id: @user.shelf.id)
-      get(:index, session: {'user_id': @user.id})
+      get :index, session: {'user_id': @user.id}
       expect(response).to have_http_status :success
       expect(assigns[:books].count).to eq 5
+    end
+  end
+
+  describe 'create' do
+    it 'should create a new book' do
+      books_attributes = {title: 'Title', author: 'Author', code: '777', shelf_id: @user.shelf.id}
+      expect {
+        post :create, params: {book: books_attributes}, session: {'user_id': @user.id}
+      }.to change(Book, :count).by 1
+    end
+
+    it 'should not create a new book with missing required params' do
+      books_attributes = {title: 'Title 2'}
+      expect {
+        post :create, params: {book: books_attributes}, session: {'user_id': @user.id}
+      }.to change(Book, :count).by 0
     end
   end
 end
